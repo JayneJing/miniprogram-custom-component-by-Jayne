2eea89d6ca99f68ec3617565e39a38c9a683da02
JayneJing <ljyhfsh1993@163.com>
2019-11-04 17:20:44 +0800

    add card component

---
+++ src/component/card/index.js
@@ -0,0 +1,12 @@
+Component({
+  properties: {
+    title: {
+      type: String,
+      value: 'Default title'
+    },
+    description: {
+      type: String,
+      value: 'Default description'
+    },
+  },
+})
